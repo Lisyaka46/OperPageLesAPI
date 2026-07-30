e95899281d17c17a22ab5f395721267fe3fb2fa9
Lisyaka46 <90254409+Lisyaka46@users.noreply.github.com>
2026-07-30 18:52:19 +0300

    Обновление мониторинга подключения к интернету

---
+++ CORE/Internet/ConnectStatus.cs
@@ -0,0 +1,65 @@
+﻿using System.Net.NetworkInformation;
+
+namespace OPLAPI.CORE.Internet
+{
+    internal readonly ref struct ConnectStatus
+    {
+        /// <summary>
+        /// Данные подключения
+        /// </summary>
+        private readonly byte[] Data;
+
+        /// <summary>
+        /// Состояние подключения к интернету
+        /// </summary>
+        public readonly bool Connection => Data[4] != 0;
+
+        /// <summary>
+        /// Количество миллисекунд потраченное на обновление подключения
+        /// </summary>
+        public readonly ushort CurrentPing => BitConverter.ToUInt16(Data, 0);
+
+        /// <summary>
+        /// Максимальный предел ожидания ответа
+        /// </summary>
+        public readonly ushort MaxPing => BitConverter.ToUInt16(Data, 2);
+
+        /// <summary>
+        /// Количество байт хранимых для состояния подключения к сети
+        /// </summary>
+        public const byte CountBytes = 5;
+
+        /// <summary>
+        /// Инициализировать объект отображающий состояние подключения к интернету
+        /// </summary>
+        /// <param name="SourceDataBytes">Байты для чтения данных</param>
+        private ConnectStatus(byte[] SourceDataBytes)
+        {
+            if (SourceDataBytes.Length != CountBytes)
+                throw new ArgumentException($"Массив байт не соответствует с ожидаемым размером ({SourceDataBytes.Length} => {CountBytes})",
+                nameof(SourceDataBytes));
+            Data = SourceDataBytes;
+        }
+
+        /// <summary>
+        /// Проверка подключения интернета
+        /// </summary>
+        internal static ConnectStatus GetInternetConnectionStatus(Ping PingData, string HostNameOrAddress, ushort MaxPing)
+        {
+            byte[] DataBytes = new byte[CountBytes];
+            try
+            {
+                PingReply reply = PingData.Send(HostNameOrAddress, MaxPing);
+                Buffer.BlockCopy(BitConverter.GetBytes((ushort)reply.RoundtripTime), 0, DataBytes, 0, sizeof(ushort));
+                DataBytes[4] = (byte)(reply.Status == IPStatus.Success ? 1 : 0);
+            }
+            catch
+            {
+                Buffer.BlockCopy(BitConverter.GetBytes(MaxPing), 0, DataBytes, 0, sizeof(ushort));
+                DataBytes[4] = 0;
+            }
+            Buffer.BlockCopy(BitConverter.GetBytes(MaxPing), 0, DataBytes, 2, sizeof(ushort));
+            return new(DataBytes);
+        }
+    }
+}
---
+++ CORE/Internet/Connection.cs
@@ -0,0 +1,75 @@
+﻿using System.Net.NetworkInformation;
+
+namespace OPLAPI.CORE.Internet
+{
+    /// <summary>
+    /// Класс управления мониторингом подключения к интернету
+    /// </summary>
+    public static class Connection
+    {
+        /// <summary>
+        /// Максимальный допустимый пинг при проверке интернета
+        /// </summary>
+        /// <remarks>
+        /// Данное свойство можно изменять даже при запущенном процессе проверки подключения
+        /// </remarks>
+        public static ushort MaxPing { get; set; } = 3000;
+
+        /// <summary>
+        /// Хост использующийся для проверки интернета
+        /// </summary>
+        /// <remarks>
+        /// Данное свойство можно изменять даже при запущенном процессе проверки подключения
+        /// </remarks>
+        public static string Host { get; set; } = "ya.ru";
+
+        /// <summary>
+        /// Текущее подключение к интернету
+        /// </summary>
+        public static bool StateConnect { get; private set; } = false;
+
+        /// <summary>
+        /// Событие изменения подключения к интернету
+        /// </summary>
+        public static event EventHandler<bool>? ConnectionChanged;
+
+        /// <summary>
+        /// Событие изменения потраченных милликенунд на подключение к интернету
+        /// </summary>
+        public static event EventHandler<ushort>? PingChanged;
+
+        /// <summary>
+        /// Запустить процесс проверки подключения к интернету
+        /// </summary>
+        /// <remarks>
+        /// Процесс является цикличным, желательно указывать <see cref="CancellationToken"/>
+        /// <code>
+        /// while (!Token.IsCancellationRequested) { ... }
+        /// </code>
+        /// </remarks>
+        /// <param name="Token">Токен для отмены операции проверки</param>
+        public static async Task StartRunTimeCheckInternetConnection(CancellationToken Token = default)
+        {
+            bool? OldValue = default;
+            using Ping PingData = new();
+            ConnectStatus SourceStatus;
+            while (!Token.IsCancellationRequested)
+            {
+                try
+                {
+                    SourceStatus = ConnectStatus.GetInternetConnectionStatus(PingData, Host, MaxPing);
+                    if (OldValue == null || OldValue != SourceStatus.Connection)
+                    {
+                        OldValue = SourceStatus.Connection;
+                        StateConnect = SourceStatus.Connection;
+                        ConnectionChanged?.Invoke(null, SourceStatus.Connection);
+                    }
+                    PingChanged?.Invoke(null, SourceStatus.CurrentPing);
+                    await Task.Delay(4000, Token);
+                }
+                catch (OperationCanceledException) { break; }
+                catch { throw; }
+            }
+        }
+    }
+}
